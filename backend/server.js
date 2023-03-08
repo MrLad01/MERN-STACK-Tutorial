397c00722cc97d98e6e0cdedb496a909e81237df
MrLad01 <ladayodeji@gmail.com>
2023-03-08 08:46:42 +0100

    debug code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const workoutRoutes = require('./routes/workouts')
 
 
 //express app
@@ -13,10 +14,7 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
-
+app.use('/api/workouts', workoutRoutes)
 
 //listen for request
 app.listen(process.env.PORT, () => {
