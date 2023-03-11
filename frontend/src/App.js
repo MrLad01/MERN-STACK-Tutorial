9716177f7e08a2e43456a698e7d838803a2bb672
MrLad01 <ladayodeji@gmail.com>
2023-03-11 16:03:53 +0100

    update on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages & components
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route 
+            path="/"
+            element={<Home />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
