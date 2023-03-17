4fbc171794b3ed51a0e761c5adc130c3d476450b
MrLad01 <ladayodeji@gmail.com>
2023-03-17 22:17:37 +0100

    update

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -7,7 +7,10 @@ export default function WorkoutDetails ({ workout }) {
     
     const handleClick = async () => {
         const response = await fetch('/api/workouts/' + workout._id, {
-            method: 'DELETE'
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${user.token}`
+            }
         })
         const json = await response.json()
 
